e87c0ac57290dada4b32bc9c97115e825ee661ba
Jekmetz <jekmetz@gmail.com>
2018-04-03 22:00:35 -0400

    Start the grayscale function

--- Main.java
+++ Main.java
@@ -5,6 +5,39 @@ public class Main{
 		ImageUtils imgu = new ImageUtils();
 		Color[][] img = imgu.loadImage("LennaCV.png");
 		imgu.addImage(img,"TestTab");
+		
+		imgu.addImage(grayScale(img,"l"),"GrayScale");
 		imgu.display();
 	}
+	
+	public static Color[][] grayScale(Color[][] img, String type){
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		int y = 0;
+
+		for(int row = 0; row < img.length; row++){
+			for(int col = 0; col < img[row].length; col++){
+				r = img[row][col].getRed();
+				g = img[row][col].getGreen();
+				b = img[row][col].getBlue();
+				
+				switch(type){
+				case "l":
+
+					break;
+
+				case "h":
+					
+					break;
+
+				default:
+
+					break;
+				}
+			}
+		}	
+		return img;
+	}
+
 }
